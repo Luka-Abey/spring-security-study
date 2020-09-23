e671241d23e70c2386b6807a5904b15189f6cb7f
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-09-23 20:33:06 +0100

    first commit!

---
+++ src/main/java/com/LukaAbey/SuperSecureAPI/SuperSecureApiApplication.java
@@ -0,0 +1,13 @@
+package com.LukaAbey.SuperSecureAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSecureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperSecureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LukaAbey/SuperSecureAPI/SuperSecureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LukaAbey.SuperSecureAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSecureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
