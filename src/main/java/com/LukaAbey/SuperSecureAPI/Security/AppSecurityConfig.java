ef262149dd75e369eb82e9cd0a20f36acca51c54
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-09-24 12:10:33 +0100

    new users

--- src/main/java/com/LukaAbey/SuperSecureAPI/Security/AppSecurityConfig.java
+++ src/main/java/com/LukaAbey/SuperSecureAPI/Security/AppSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.LukaAbey.SuperSecureAPI.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +18,13 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().httpBasic();
 
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails Johnny = User.builder().username("JohnnySmith").password("password").roles("STUDENT").build();
+
+		return new InMemoryUserDetailsManager(Johnny);
 	}
 }
