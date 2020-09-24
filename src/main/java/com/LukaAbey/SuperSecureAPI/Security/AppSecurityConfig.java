66cb468990009d6704f6dfa221fc39d94d856a97
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-09-24 12:20:59 +0100

    BCrypt working

--- src/main/java/com/LukaAbey/SuperSecureAPI/Security/AppSecurityConfig.java
+++ src/main/java/com/LukaAbey/SuperSecureAPI/Security/AppSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.LukaAbey.SuperSecureAPI.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +9,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
-	//
+
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	public AppSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().httpBasic();
@@ -23,7 +32,8 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	@Bean
 	protected UserDetailsService userDetailsService() {
-		UserDetails Johnny = User.builder().username("JohnnySmith").password("password").roles("STUDENT").build();
+		UserDetails Johnny = User.builder().username("JohnnySmith").password(passwordEncoder.encode("password"))
+				.roles("STUDENT").build();
 
 		return new InMemoryUserDetailsManager(Johnny);
 	}
