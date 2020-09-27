eceaa82e6ec113f467c47641724056f40e0af800
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-09-27 22:17:37 +0100

    learning csrfs

--- src/main/java/com/LukaAbey/Student/StudentManagementController.java
+++ src/main/java/com/LukaAbey/Student/StudentManagementController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("management/api/v1/students")
 public class StudentManagementController {
-	private static final List<Student> STUDENTS = Arrays.asList(new Student(4, "Jo"), new Student(5, "Harry"));
+	private static final List<Student> STUDENTS = Arrays.asList(new Student(4, "Joo"), new Student(5, "Harry"));
 
 	@GetMapping
 	public List<Student> getAllStudents() {
